00da59cd09823f4e83d804100b16755157d027cf
Дмитрий <1917-01_1917@mail.ru>
2022-02-01 20:01:05 +0400

    Bonus miles

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 3347;
+        int theCostOfTheBonusMile = 20;
+        int bonus = ticketPrice / theCostOfTheBonusMile;
+        System.out.println(" Здравствуйте. Благодарим вас, что выбрали нашу авиакомпанию.\n Ваш бонус составляет: " + bonus + " миль");
+    }
+}
